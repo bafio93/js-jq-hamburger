cb3750acfd7f74445291b376f054dc194218ec92
Previtali_Fabio <fabprev93@gmail.com>
2019-11-26 08:17:21 +0100

    Done!

---
+++ main.js
@@ -0,0 +1,7 @@
+// $(".hamburger-menu").addClass("active");
+$(".fa-bars").click(function(){
+    $(".hamburger-menu").addClass("active");
+});
+$(".fa-times").click(function(){
+    $(".hamburger-menu").removeClass("active");
+});
